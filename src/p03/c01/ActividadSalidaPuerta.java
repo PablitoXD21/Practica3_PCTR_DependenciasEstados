df1cffdcb9b3c7b0a98fac13a1eb04ab0778abaf
Inigo <Inigo@10.160.43.207>
2022-03-23 10:03:15 +0100

    Codigo salida terminado

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,5 +1,10 @@
 package src.p03.c01;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Permite sacar a los usuarios que hay en el parque
  * 
@@ -7,7 +12,7 @@ package src.p03.c01;
  *
  */
 
-public class ActividadSalidaPuerta{
+public class ActividadSalidaPuerta implements Runnable{
 	
 	private static final int NUMSALIDAS = 20;
 	private String puerta;
@@ -23,5 +28,19 @@ public class ActividadSalidaPuerta{
 		
 		this.puerta = puerta;
 		this.parque = parque;
+	}
+	
+	@Override
+	public void run() {
+		for (int i = 0; i < NUMSALIDAS; i++) {
+			try {
+				parque.entrarAlParque(puerta);
+				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
+			} catch (InterruptedException e) {
+				Logger.getGlobal().log(Level.INFO, "Salida interrumpida");
+				Logger.getGlobal().log(Level.INFO, e.toString());
+				return;
+			}
+		}
 	}
 }
