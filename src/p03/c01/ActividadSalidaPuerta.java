83ca201165397febe8719cb507631f178052fa72
Inigo <Inigo@10.160.43.207>
2022-03-23 09:55:02 +0100

    Codigo de la actividad de salida

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,8 +1,21 @@
 package src.p03.c01;
 
-public class ActividadSalidaPuerta{
+/**
+ * Permite sacar a los usuarios que hay en el parque
+ * 
+ * @author Inigo y Pablo
+ *
+ */
 
-	//
-	// TODO
-	//
+public class ActividadSalidaPuerta{
+	
+	private static final int NUMSALIDAS = 20;
+	private String puerta;
+	private IParque parque;
+	
+	public ActividadSalidaPuerta(String puerta, IParque parque) {
+		
+		this.puerta = puerta;
+		this.parque = parque;
+	}
 }
