03f2176dc956391da255640567a893a97b52e0a9
Pablo <pzb1001@alu.ubu.es>
2022-03-30 17:52:48 +0200

    Solucionando Errores

--- src/p03/c01/Parque.java
+++ src/p03/c01/Parque.java
@@ -20,11 +20,11 @@ public class Parque implements IParque{
 
 	@Override
 	public void entrarAlParque(String puerta){		// TODO
-		comprobarAntesDeEntrar();
+
 		// Si no hay entradas por esa puerta, inicializamos
 		if(puertas.get(puerta) == null)
 			puertas.put(puerta, 0);
-		
+		comprobarAntesDeEntrar();
 		// Aumentamos el contador total y el individual
 		// imprimir Mensaje de entrada
 		System.out.println("Entrada al parque por la puerta "+ puerta);
@@ -92,7 +92,8 @@ public class Parque implements IParque{
 
 	@Override
 	public void salirDelParque(String puerta) {
-		
+		if(puertas.get(puerta) == null)
+			puertas.put(puerta, 0);
 		comprobarAntesDeSalir();
 
 		//Actializar contadores
